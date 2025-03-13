fe69acd8a90962a5ee26979829aab035b29aea8f
Emanuel Sanchez <emanuelsanchez3303@gmail.com>
2025-03-13 01:06:04 -0500

    Adds to main: - Find if folder is made, if not make folder - Find if files are made, if not make files (admins, employees, properties, log)

--- AttendanceV2.cpp
+++ AttendanceV2.cpp
@@ -1,10 +1,37 @@
 #include "AttendanceV2.h"
 
+
 using namespace System;
 using namespace System::Windows::Forms;
 void main(array<String^>^ args) {
+
+
+	// Making the files needed if not already made
+	using namespace std;
+	system("mkdir C:\\Attendance");
+
+	ifstream checkingLocation;
+	ofstream makingLocation;
+	
+	byte const filesAmount = 4;
+	string fileNames[filesAmount] = { "admins", "employees", "properties", "log"};
+
+	// Checking for .txt files
+	for (byte i = 0; i < filesAmount; i++) {
+		checkingLocation.open("C:\\Attendance\\" + fileNames[i] + ".txt");
+		if (!checkingLocation) {
+			ofstream makingLocation("C:\\Attendance\\" + fileNames[i] + ".txt");
+			makingLocation.close();
+		}
+		else {
+			checkingLocation.close();
+		}
+	}
+	
+
+	// Starting GUI
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
-	AttendanceV2::AttendanceV2 form;                   // ProjectCPP1::MainForm form;
+	AttendanceV2::AttendanceV2 form;                 
 	Application::Run(% form);
 }
--- AttendanceV2.h
+++ AttendanceV2.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <fstream>
+#include <iostream>
+#include <Windows.h>
 
 namespace AttendanceV2 {
 
@@ -22,8 +25,8 @@ namespace AttendanceV2 {
 				delete components;
 			
 		}
-	private: System::Windows::Forms::Panel^ panel1;
-	private: System::Windows::Forms::Label^ label1;
+
+
 	protected:
 
 	private:
@@ -31,36 +34,15 @@ namespace AttendanceV2 {
 
 #pragma region Windows Form Designer generated code
 		void InitializeComponent(void){
-			this->panel1 = (gcnew System::Windows::Forms::Panel());
-			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->panel1->SuspendLayout();
 			this->SuspendLayout();
-			// 
-			// panel1
-			// 
-			this->panel1->Controls->Add(this->label1);
-			this->panel1->Location = System::Drawing::Point(108, 79);
-			this->panel1->Name = L"panel1";
-			this->panel1->Size = System::Drawing::Size(301, 206);
-			this->panel1->TabIndex = 0;
-			// 
-			// label1
-			// 
-			this->label1->Location = System::Drawing::Point(66, 78);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(168, 26);
-			this->label1->TabIndex = 0;
-			this->label1->Text = L"testing pushing into git";
 			// 
 			// AttendanceV2
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(560, 379);
-			this->Controls->Add(this->panel1);
 			this->Name = L"AttendanceV2";
 			this->Text = L"Attendance";
-			this->panel1->ResumeLayout(false);
 			this->ResumeLayout(false);
 
 		}
